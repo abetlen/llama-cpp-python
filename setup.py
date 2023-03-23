93f074a4fad22bbc8c7ed76fe207828cfbd0e209
Andrei Betlen <abetlen@gmail.com>
2023-03-23 14:24:08 -0400

    Initial pypi release

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from skbuild import setup
 
 setup(
-    name="llama_cpp",
+    name="llama_cpp_python",
     description="A Python wrapper for llama.cpp",
     version="0.1.1",
     author="Andrei Betlen",
     author_email="abetlen@gmail.com",
     license="MIT",
-    packages=['llama_cpp'],
+    packages=["llama_cpp"]
 )
