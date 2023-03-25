4250380c0acd58d29c92fa14fd812508677697ca
Andrei Betlen <abetlen@gmail.com>
2023-03-25 16:26:35 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from skbuild import setup
 setup(
     name="llama_cpp_python",
     description="A Python wrapper for llama.cpp",
-    version="0.1.5",
+    version="0.1.6",
     author="Andrei Betlen",
     author_email="abetlen@gmail.com",
     license="MIT",
