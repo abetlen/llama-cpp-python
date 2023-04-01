5f2e822b5957e87726678f417c102794d5ed9f31
Andrei Betlen <abetlen@gmail.com>
2023-04-01 13:01:45 -0400

    Rename inference example

--- examples/high_level_api_inference.py
+++ examples/high_level_api_inference.py
@@ -4,14 +4,14 @@ import argparse
 from llama_cpp import Llama
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--model", type=str, default=".//models/...")
+parser.add_argument("-m", "--model", type=str, default="./models/...")
 args = parser.parse_args()
 
 llm = Llama(model_path=args.model)
 
 output = llm(
     "Question: What are the names of the planets in the solar system? Answer: ",
-    max_tokens=48,
+    max_tokens=1,
     stop=["Q:", "\n"],
     echo=True,
 )
