542cf4fd56b058e5dedb71a0033f560ad9aa8bdf
Gary Mulder <gjmulder@gmail.com>
2023-04-15 14:50:02 +0000

    Added response times

--- examples/tele-llama.py
+++ examples/tele-llama.py
@@ -1,5 +1,5 @@
 import os
-# import telethon
+import time
 import logging
 from telethon import TelegramClient, events #, types
 import requests
@@ -32,7 +32,7 @@ def get_weather(city, api_key):
     else:
         print("City not found")
         
-model = "Alpacabot 30B"
+model = "Alpacabot 7B"
 LLAMA_HOST=os.environ.get("LLAMA_HOST")
 api_url = f'http://{LLAMA_HOST}:8000/v1/chat/completions'
 
@@ -62,7 +62,7 @@ async def beth(event):
             return
 
         # Get the message text
-        message_text = event.message.message.replace('/beth ', '').strip()
+        message_text = event.message.message.replace('/beth', '').strip()
         parts = message_text.split(' ')
         logger.debug(f"Parts: {parts}")
         if len(parts) >= 2:
@@ -82,7 +82,8 @@ async def beth(event):
         headers = {'accept': 'application/json', 'Content-Type': 'application/json'}
         data = {
             "messages": [
-                {"role": "system", "content": "You are a female grandmaster chess player named Beth who needs drugs to play well"},
+                {"role": "system",
+                 "content": "Hello, I'm Beth Harmon, a female chess prodigy. My mind moves faster than the pieces on the board, and I need to be at my best to win. Sometimes, I rely on a little help to get me there."},
                 {"role": "user", "content": message_text}
             ],
             "temperature": temperature
@@ -91,8 +92,18 @@ async def beth(event):
         # Log the user prompt
         logger.info(f"Temp: {temperature}, prompt: {message_text}")
 
+        # Record the time before the API request
+        start_time = time.time()
+
         # Send the API request
         response = requests.post(api_url, headers=headers, json=data)
+
+        # Record the time after the API request
+        end_time = time.time()
+
+        # Calculate the time difference
+        time_difference = end_time - start_time
+        minutes, seconds = divmod(time_difference, 60)
 
         # Check if the request was successful
         if response.status_code == 200:
@@ -104,10 +115,16 @@ async def beth(event):
             if not result_text:
                 result_text = "I'm sorry, but it is still your turn to move."
 
-            # Log the API response
-            logger.info(f"API response: {result_text}")
+            # Format the response time
+            response_time = f"({int(minutes)}m{int(seconds)}s)"
 
-            await client.send_message(event.chat_id, result_text)
+            # Add the response time to the result text
+            result_text_with_time = f"{result_text} {response_time}"
+
+            # Log the API response
+            logger.info(f"API response: {result_text_with_time}")
+
+            await client.send_message(event.chat_id, result_text_with_time)
         else:
             # Send an error message if the request was not successful
             await client.send_message(event.chat_id, "Sorry, I need to go to the bathroom. Back soon!")
@@ -120,76 +137,3 @@ async def beth(event):
 # Start the Telegram client
 logger.info("Starting Telegram bot with LLama model: %s ", model)
 client.run_until_disconnected()
-
-# # Create an event handler for /gpt command
-# @client.on(events.NewMessage(pattern='/gpt'))
-# async def gpt(event):
-#     global message_count
-#
-#     try:
-#         # Check if message count has reached limit
-#         if message_count >= 1000:
-#             # Send "tired" message and return
-#             await client.send_message(event.chat_id, "I'm tired, I can't answer any more messages.")
-#             return
-#
-#         # Increment message count
-#         message_count += 1
-#
-#         # Set the default temperature value to 0.7
-#         temperature = 0.7
-#
-#         # Get the prompt text
-#         prompt = event.text.split(' ', 1)[1] if len(event.text.split()) > 1 else ""
-#
-#         # Check if the prompt has a temperature value
-#         if prompt and len(prompt.split()) > 1:
-#             try:
-#                 # Get the temperature value
-#                 temperature = float(prompt.split()[0])
-#
-#                 # Check if the temperature is within the valid range of 0.0 to 2.0
-#                 if temperature < 0.0 or temperature > 2.0:
-#                     raise ValueError
-#             except ValueError:
-#                 # Handle invalid temperature value
-#                 await client.send_message(event.chat_id, "Please provide a valid temperature value between 0.0 and 2.0.")
-#                 return
-#
-#             # Get the prompt text without the temperature value
-#             prompt = ' '.join(prompt.split()[1:])
-#
-#         # Call the OpenAI GPT API
-#         try:
-#             response = openai.Completion.create(engine=model, prompt=prompt, max_tokens=1024, temperature=temperature)
-#             answer = "[%s] %s" % (model, response.choices[0].text)
-#         except openai.error.OpenAIError as e:
-#             # Handle OpenAI API errors
-#             answer = "Sorry, I couldn't generate a response. Please try again later."
-#             logger.error("OpenAI API error: %s", str(e))
-#         except Exception as e:
-#             # Handle other errors
-#             answer = "Sorry, an unexpected error occurred. Please try again later."
-#             logger.error("Unexpected error: %s", str(e))
-#
-#         # Post the GPT response to the Telegram group or direct message
-#         try:
-#             await client.send_message(event.chat_id, answer)
-#             logger.info("Prompt: %s", prompt)
-#             logger.info("%s - %s" % (message_count, answer))
-#         except telethon.errors.rpcerrorlist.RPCError as e:
-#             # Handle Telegram API errors
-#             error_message = "Sorry, there was an error sending the message. Please try again later."
-#             await client.send_message(event.chat_id, error_message)
-#             logger.error("Telegram API error: %s", str(e))
-#         except Exception as e:
-#             # Handle other errors
-#             error_message = "Sorry, an unexpected error occurred. Please try again later."
-#             await client.send_message(event.chat_id, error_message)
-#             logger.error("Unexpected error: %s", str(e))
-#
-#     except Exception as e:
-#         # Handle unexpected errors
-#         logger.error("Unexpected error: %s", str(e))
-
-
