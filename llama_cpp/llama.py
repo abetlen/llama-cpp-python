33bab8d4c14b3f17f086b5f8e235affb3f6b0379
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2023-05-07 22:24:12 +0000

    Mechanism to retrieve all logprobs on completion.

--- llama_cpp/llama.py
+++ llama_cpp/llama.py
@@ -865,6 +865,7 @@ class Llama:
         min_p: float = 0.05,
         typical_p: float = 1.0,
         logprobs: Optional[int] = None,
+        full_logprobs: bool = False,
         echo: bool = False,
         stop: Optional[Union[str, List[str]]] = [],
         frequency_penalty: float = 0.0,
@@ -1284,7 +1285,18 @@ class Llama:
             text_str = text_str + suffix
 
         logprobs_or_none: Optional[CompletionLogprobs] = None
-        if logprobs is not None:
+        if logprobs is not None and full_logprobs:
+            if logprobs == 0:
+                logits = self.eval_logits
+            elif logprobs == -1:
+                logits = list(self.eval_logits)[-len(completion_tokens):]
+            else:
+                logits = list(self.eval_logits)[-logprobs:]
+            logprobs_or_none = [
+                Llama.logits_to_logprobs(list(map(float, row)))
+                for row in logits
+            ]
+        elif logprobs is not None:
             text_offset = 0 if echo else len(prompt)
             token_offset = 0 if echo else len(prompt_tokens[1:])
             text_offsets: List[int] = []
@@ -1373,6 +1385,7 @@ class Llama:
         min_p: float = 0.05,
         typical_p: float = 1.0,
         logprobs: Optional[int] = None,
+        full_logprobs: bool = False,
         echo: bool = False,
         stop: Optional[Union[str, List[str]]] = [],
         frequency_penalty: float = 0.0,
@@ -1402,6 +1415,7 @@ class Llama:
             min_p: The min-p value to use for minimum p sampling. Minimum P sampling as described in https://github.com/ggerganov/llama.cpp/pull/3841
             typical_p: The typical-p value to use for sampling. Locally Typical Sampling implementation described in the paper https://arxiv.org/abs/2202.00666.
             logprobs: The number of logprobs to return. If None, no logprobs are returned.
+            full_logprobs: if `logprobs` is not None, returns full logprobs vector (instead of top). If `logprobs==-1` only generated tokens, `logprobs==0` all tokens, else last `logprobs` tokens.
             echo: Whether to echo the prompt.
             stop: A list of strings to stop generation when encountered.
             frequency_penalty: The penalty to apply to tokens based on their frequency in the prompt.
@@ -1436,6 +1450,7 @@ class Llama:
             min_p=min_p,
             typical_p=typical_p,
             logprobs=logprobs,
+            full_logprobs=full_logprobs,
             echo=echo,
             stop=stop,
             frequency_penalty=frequency_penalty,
@@ -1470,6 +1485,7 @@ class Llama:
         min_p: float = 0.05,
         typical_p: float = 1.0,
         logprobs: Optional[int] = None,
+        full_logprobs: bool = False,
         echo: bool = False,
         stop: Optional[Union[str, List[str]]] = [],
         frequency_penalty: float = 0.0,
@@ -1499,6 +1515,7 @@ class Llama:
             min_p: The min-p value to use for minimum p sampling. Minimum P sampling as described in https://github.com/ggerganov/llama.cpp/pull/3841
             typical_p: The typical-p value to use for sampling. Locally Typical Sampling implementation described in the paper https://arxiv.org/abs/2202.00666.
             logprobs: The number of logprobs to return. If None, no logprobs are returned.
+            full_logprobs: if `logprobs` is not None, returns full logprobs vector (instead of top). If `logprobs==-1` only generated tokens, `logprobs==0` all tokens, else last `logprobs` tokens.
             echo: Whether to echo the prompt.
             stop: A list of strings to stop generation when encountered.
             frequency_penalty: The penalty to apply to tokens based on their frequency in the prompt.
@@ -1533,6 +1550,7 @@ class Llama:
             min_p=min_p,
             typical_p=typical_p,
             logprobs=logprobs,
+            full_logprobs=full_logprobs,
             echo=echo,
             stop=stop,
             frequency_penalty=frequency_penalty,
