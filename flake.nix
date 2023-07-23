ad392050367ababbdff0767b2e008ae11a0099a5
Charles Duffy <charles@dyfis.net>
2023-07-23 13:14:15 -0500

    flake.nix: build from wheels rather than source

--- flake.nix
+++ flake.nix
@@ -15,25 +15,20 @@
         poetryEnv =  pkgs.poetry2nix.mkPoetryEnv {
           projectDir = self;
           python = pkgs.python310;
-          extras = [ ]; # "server" broken for now, see FIXME below
+          extras = [ "server" ];
           groups = [ "dev" ];
+          preferWheels = true;
           overrides = let
             extraDepsNeeded = {
-              annotated-types = [ "hatchling" ];
-              maturin = [ "setuptools" ];
-              mkdocs-material = [ "hatch-requirements-txt" ];
-              mkdocstrings = [ "pdm-backend" ];
-              mkdocstrings-python = null; # FIXME: recursion error in poetry2nix needs running down
-              pydantic-core = [ "maturin" ];
-              scikit-build = [ "hatch-fancy-pypi-readme" "hatch-vcs" "hatchling" ];
+              mkdocstrings-python = (self: super: null); # FIXME: recursion error in poetry2nix needs running down
             };
           in (pkgs.poetry2nix.defaultPoetryOverrides.extend (self: super:
             builtins.mapAttrs (package: build-requirements:
               if builtins.typeOf build-requirements == "list"
               then (builtins.getAttr package super).overridePythonAttrs (old: {
-                buildInputs = (old.buildInputs or []) ++ (builtins.map (pkg: if builtins.isString pkg then builtins.getAttr pkg super else pkg) build-requirements);
+                buildInputs = (old.buildInputs or []) ++ (builtins.map (pkg: if builtins.isString pkg then builtins.getAttr pkg self else pkg) build-requirements);
               })
-              else build-requirements
+              else (build-requirements self super)
             ) extraDepsNeeded));
         };
       in {
@@ -51,4 +46,3 @@
         };
       });
 }
-
