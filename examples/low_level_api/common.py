1eaace8ea394423df6185f2cc83e7cb98a4bc35b
anil <412569+aniljava@users.noreply.github.com>
2024-01-15 09:46:35 -0600

    Fix low_level_api_chat_cpp example to match current API (#1086)
    * Fix low_level_api_chat_cpp to match current API
    
    * Fix low_level_api_chat_cpp to match current API
    
    * Using None instead of empty string to so that default prompt template can be used if no prompt provided
    
    ---------
    
    Co-authored-by: Anil Pathak <anil@heyday.com>

--- examples/low_level_api/common.py
+++ examples/low_level_api/common.py
@@ -106,7 +106,7 @@ def gpt_params_parse(argv = None):
     parser.add_argument("--mirostat_lr", type=float, default=0.1, help="Mirostat learning rate, parameter eta",dest="mirostat_eta")
 
     parser.add_argument("-m", "--model", type=str, default="./models/llama-7B/ggml-model.bin", help="model path",dest="model")
-    parser.add_argument("-p", "--prompt", type=str, default="", help="initial prompt",dest="prompt")
+    parser.add_argument("-p", "--prompt", type=str, default=None, help="initial prompt",dest="prompt")
     parser.add_argument("-f", "--file", type=str, default=None, help="file containing initial prompt to load",dest="file")
     parser.add_argument("--session", type=str, default=None, help="file to cache model state in (may be large!)",dest="path_session")
     parser.add_argument("--in-prefix", type=str, default="", help="string to prefix user inputs with", dest="input_prefix")
