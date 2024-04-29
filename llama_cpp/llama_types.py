22dc26219e2e1d5c8485f113532d6fc0e09b29d2
JDScript <jd@jdscript.net>
2024-04-30 04:28:01 +0800

    fix: add binding for name in ChatCompletionRequestToolMessage

--- llama_cpp/llama_types.py
+++ llama_cpp/llama_types.py
@@ -222,6 +222,7 @@ class ChatCompletionRequestToolMessage(TypedDict):
     role: Literal["tool"]
     content: Optional[str]
     tool_call_id: str
+    name: Optional[str]
 
 
 class ChatCompletionRequestFunctionMessage(TypedDict):
