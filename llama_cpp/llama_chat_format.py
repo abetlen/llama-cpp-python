dd47dda13fc269eb3a6fc3ab5503d4ef8b93257d
Andrei Betlen <abetlen@gmail.com>
2024-04-30 00:49:50 -0400

    Remove unnecessary import

--- llama_cpp/llama_chat_format.py
+++ llama_cpp/llama_chat_format.py
@@ -2279,7 +2279,6 @@ class Llava15ChatHandler:
         llama_types.CreateChatCompletionResponse,
         Iterator[llama_types.CreateChatCompletionStreamResponse],
     ]:
-        from contextlib import ExitStack    
         assert (
             llama.context_params.logits_all is True
         )  # BUG: logits_all=True is required for llava
