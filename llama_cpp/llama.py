a5b2efc1428dcfa884f99d7d64afe5cee2a5f4d0
Andrei Betlen <abetlen@gmail.com>
2024-05-09 09:47:02 -0400

    Don't expose internal chat handlers yet

--- llama_cpp/llama.py
+++ llama_cpp/llama.py
@@ -378,7 +378,7 @@ class Llama:
 
         self.chat_format = chat_format
         self.chat_handler = chat_handler
-        self.chat_handlers = {}
+        self._chat_handlers: Dict[str, llama_chat_format.LlamaChatCompletionHandler] = {}
 
         self.draft_model = draft_model
 
@@ -426,7 +426,7 @@ class Llama:
             print(f"Available chat formats from metadata: {', '.join(template_choices.keys())}", file=sys.stderr)
 
         for name, template in template_choices.items():
-            self.chat_handlers[name] = llama_chat_format.Jinja2ChatFormatter(
+            self._chat_handlers[name] = llama_chat_format.Jinja2ChatFormatter(
                 template=template,
                 eos_token=eos_token,
                 bos_token=bos_token,
@@ -1725,7 +1725,7 @@ class Llama:
         Returns:
             Generated chat completion or a stream of chat completion chunks.
         """
-        handler = self.chat_handler or self.chat_handlers.get(self.chat_format) or llama_chat_format.get_chat_completion_handler(
+        handler = self.chat_handler or self._chat_handlers.get(self.chat_format) or llama_chat_format.get_chat_completion_handler(
             self.chat_format
         )
         return handler(
