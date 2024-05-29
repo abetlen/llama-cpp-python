fce73d0919912f3b6e9dcb009f4d621de05d7e98
Sigbjørn Skjæret <sigbjorn.skjaeret@scala.com>
2024-05-29 21:55:43 +0200

    don't add bos/eos on non-infill pre-tokenized prompt

--- llama_cpp/llama.py
+++ llama_cpp/llama.py
@@ -971,8 +971,8 @@ class Llama:
         prefix_token_id: int = self._model.token_prefix()
         middle_token_id: int = self._model.token_middle()
         suffix_token_id: int = self._model.token_suffix()
-        bos_tokens: List[int] = [self.token_bos()] if self._model.add_bos_token() != 0 else []
-        eos_tokens: List[int] = [self.token_eos()] if self._model.add_eos_token() == 1 else []
+        bos_tokens: List[int] = [self.token_bos()] if not (isinstance(prompt, list) and suffix is None) and self._model.add_bos_token() != 0 else []
+        eos_tokens: List[int] = [self.token_eos()] if not (isinstance(prompt, list) and suffix is None) and self._model.add_eos_token() == 1 else []
         # If prompt is empty, initialize completion with BOS token to avoid
         # detokenization including a space at the beginning of the completion
         completion_tokens: List[int] = [] if len(prompt) > 0 else [self.token_bos()]
