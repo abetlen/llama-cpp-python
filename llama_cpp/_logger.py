f9db9b74f1426d226c84f0f9fa22063ae13b2359
Andrei Betlen <abetlen@gmail.com>
2024-09-15 16:13:10 -0400

    Fix logger

--- llama_cpp/_logger.py
+++ llama_cpp/_logger.py
@@ -5,21 +5,24 @@ import logging
 import llama_cpp
 
 # enum ggml_log_level {
-#     GGML_LOG_LEVEL_ERROR = 2,
-#     GGML_LOG_LEVEL_WARN = 3,
-#     GGML_LOG_LEVEL_INFO = 4,
-#     GGML_LOG_LEVEL_DEBUG = 5
+#     GGML_LOG_LEVEL_NONE  = 0,
+#     GGML_LOG_LEVEL_INFO  = 1,
+#     GGML_LOG_LEVEL_WARN  = 2,
+#     GGML_LOG_LEVEL_ERROR = 3,
+#     GGML_LOG_LEVEL_DEBUG = 4,
 # };
 GGML_LOG_LEVEL_TO_LOGGING_LEVEL = {
-    2: logging.ERROR,
-    3: logging.WARNING,
-    4: logging.INFO,
-    5: logging.DEBUG,
+    0: logging.CRITICAL,
+    1: logging.INFO,
+    2: logging.WARNING,
+    3: logging.ERROR,
+    4: logging.DEBUG,
 }
 
 logger = logging.getLogger("llama-cpp-python")
 
 
+# typedef void (*ggml_log_callback)(enum ggml_log_level level, const char * text, void * user_data);
 @llama_cpp.llama_log_callback
 def llama_log_callback(
     level: int,
