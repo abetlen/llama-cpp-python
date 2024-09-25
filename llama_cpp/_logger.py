926b4143e865bc6ffd405a952639099214385139
Andrei Betlen <abetlen@gmail.com>
2024-09-25 13:23:18 -0400

    feat: Update llama.cpp

--- llama_cpp/_logger.py
+++ llama_cpp/_logger.py
@@ -10,6 +10,7 @@ import llama_cpp
 #     GGML_LOG_LEVEL_WARN  = 2,
 #     GGML_LOG_LEVEL_ERROR = 3,
 #     GGML_LOG_LEVEL_DEBUG = 4,
+#     GGML_LOG_LEVEL_CONT  = 5, // continue previous log
 # };
 GGML_LOG_LEVEL_TO_LOGGING_LEVEL = {
     0: logging.CRITICAL,
@@ -17,10 +18,12 @@ GGML_LOG_LEVEL_TO_LOGGING_LEVEL = {
     2: logging.WARNING,
     3: logging.ERROR,
     4: logging.DEBUG,
+    5: logging.DEBUG,
 }
 
 logger = logging.getLogger("llama-cpp-python")
 
+_last_log_level = GGML_LOG_LEVEL_TO_LOGGING_LEVEL[0]
 
 # typedef void (*ggml_log_callback)(enum ggml_log_level level, const char * text, void * user_data);
 @llama_cpp.llama_log_callback
@@ -29,8 +32,12 @@ def llama_log_callback(
     text: bytes,
     user_data: ctypes.c_void_p,
 ):
+    # TODO: Correctly implement continue previous log
+    global _last_log_level
+    log_level = GGML_LOG_LEVEL_TO_LOGGING_LEVEL[level] if level != 5 else _last_log_level
     if logger.level <= GGML_LOG_LEVEL_TO_LOGGING_LEVEL[level]:
         print(text.decode("utf-8"), end="", flush=True, file=sys.stderr)
+    _last_log_level = log_level
 
 
 llama_cpp.llama_log_set(llama_log_callback, ctypes.c_void_p(0))
