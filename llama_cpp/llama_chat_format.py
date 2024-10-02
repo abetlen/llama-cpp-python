08cc0b822b57df86dba43be1f04935ae67177304
Andrei Betlen <abetlen@gmail.com>
2024-10-02 04:39:24 -0400

    Add Paligemma support

--- llama_cpp/llama_chat_format.py
+++ llama_cpp/llama_chat_format.py
@@ -28,9 +28,11 @@ from jinja2.sandbox import ImmutableSandboxedEnvironment
 import numpy as np
 import numpy.typing as npt
 
+import llama_cpp
 import llama_cpp.llama as llama
 import llama_cpp.llama_types as llama_types
 import llama_cpp.llama_grammar as llama_grammar
+import llama_cpp._internals as internals
 
 from ._logger import logger
 from ._utils import suppress_stdout_stderr, Singleton
@@ -3349,6 +3351,204 @@ class MiniCPMv26ChatHandler(Llava15ChatHandler):
         "{% endif %}"
     )
 
+
+class PaligemmaChatHandler(Llava15ChatHandler):
+    def __call__(
+        self,
+        *,
+        llama: llama.Llama,
+        messages: List[llama_types.ChatCompletionRequestMessage],
+        functions: Optional[List[llama_types.ChatCompletionFunction]] = None,
+        function_call: Optional[llama_types.ChatCompletionRequestFunctionCall] = None,
+        tools: Optional[List[llama_types.ChatCompletionTool]] = None,
+        tool_choice: Optional[llama_types.ChatCompletionToolChoiceOption] = None,
+        temperature: float = 0.2,
+        top_p: float = 0.95,
+        top_k: int = 40,
+        min_p: float = 0.05,
+        typical_p: float = 1.0,
+        stream: bool = False,
+        stop: Optional[Union[str, List[str]]] = [],
+        seed: Optional[int] = None,
+        response_format: Optional[
+            llama_types.ChatCompletionRequestResponseFormat
+        ] = None,
+        max_tokens: Optional[int] = None,
+        presence_penalty: float = 0.0,
+        frequency_penalty: float = 0.0,
+        repeat_penalty: float = 1.1,
+        tfs_z: float = 1.0,
+        mirostat_mode: int = 0,
+        mirostat_tau: float = 5.0,
+        mirostat_eta: float = 0.1,
+        model: Optional[str] = None,
+        logits_processor: Optional[llama.LogitsProcessorList] = None,
+        grammar: Optional[llama.LlamaGrammar] = None,
+        logit_bias: Optional[Dict[str, float]] = None,
+        logprobs: Optional[bool] = None,
+        top_logprobs: Optional[int] = None,
+        **kwargs,  # type: ignore
+    ) -> Union[
+        llama_types.CreateChatCompletionResponse,
+        Iterator[llama_types.CreateChatCompletionStreamResponse],
+    ]:
+        assert self.clip_ctx is not None
+
+        if len(messages) != 1:
+            raise ValueError("PaligemmaChatHandler only supports single-turn conversations.")
+
+        image_urls = self.get_image_urls(messages)
+
+        if len(image_urls) > 1:
+            raise ValueError("PaligemmaChatHandler only supports single image per turn.")
+
+        text = "<s>answer en "
+        message = messages[0]
+        if isinstance(message["content"], str):
+            text = message["content"]
+        elif isinstance(message["content"], list):
+            for content in message["content"]:
+                if content["type"] == "text":
+                    text += content["text"]
+        text += "\n"
+
+        if self.verbose:
+            print(text, file=sys.stderr)
+
+
+
+        tokens = llama.tokenize(text.encode("utf-8"), special=True)
+        embedding_dim = llama_cpp.llama_n_embd(llama.model)
+        tokens_np = np.array(tokens).astype(np.int32)
+        token_embedding = np.empty((len(tokens), embedding_dim), dtype=np.single)
+        llama_cpp.llama_token_inp_embd(
+            llama.ctx,
+            tokens_np.ctypes.data_as(ctypes.POINTER(ctypes.c_int32)),
+            len(tokens),
+            token_embedding.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+        )
+
+        if len(image_urls) > 0:
+            image_embedding = self._embed_image_bytes(self.load_image(image_urls[0]))
+            n_image_pos = image_embedding.contents.n_image_pos
+            embeds = np.concatenate([np.ctypeslib.as_array(image_embedding.contents.embed, shape=(n_image_pos, embedding_dim)), token_embedding], axis=0)
+            n_tokens = n_image_pos + len(tokens)
+            llama.input_ids[: n_tokens] = (
+                llama.tokenize(b"<image>", add_bos=False, special=True) * image_embedding.contents.n_image_pos + tokens
+            )
+        else:
+            n_tokens = len(tokens)
+            llama.input_ids[: n_tokens] = tokens
+            embeds = token_embedding
+
+
+        n_batch = 512
+        batch = internals.LlamaBatch(n_tokens=n_batch, embd=embedding_dim, n_seq_max=1)
+
+        batch.batch.n_tokens = n_tokens
+
+        np.ctypeslib.as_array(batch.batch.embd, shape=(n_batch, embedding_dim))[
+            :n_tokens, :
+        ] = embeds
+        np.ctypeslib.as_array(batch.batch.pos, shape=(n_batch,))[:n_tokens] = np.arange(n_tokens)
+        np.ctypeslib.as_array(batch.batch.n_seq_id, shape=(n_batch,))[:] = 1
+        np.ctypeslib.as_array(batch.batch.logits, shape=(n_batch,))[:] = False
+        np.ctypeslib.as_array(batch.batch.logits, shape=(n_batch,))[n_tokens - 1] = True
+
+        for i in range(n_tokens):
+            batch.batch.seq_id[i][0] = 0
+
+        # Evaluate prompt
+        llama.reset()
+        llama._ctx.kv_cache_clear()
+        llama_cpp.llama_set_causal_attn(llama._ctx.ctx, False)
+        llama._ctx.decode(batch)
+        llama.n_tokens += n_tokens
+        llama_cpp.llama_set_causal_attn(llama._ctx.ctx, True)
+
+        # Get prompt tokens to avoid a cache miss
+        prompt = llama.input_ids[: llama.n_tokens].tolist()
+
+        if response_format is not None and response_format["type"] == "json_object":
+            grammar = _grammar_for_response_format(response_format)
+
+        # Convert legacy functions to tools
+        if functions is not None:
+            tools = [
+                {
+                    "type": "function",
+                    "function": function,
+                }
+                for function in functions
+            ]
+
+        # Convert legacy function_call to tool_choice
+        if function_call is not None:
+            if isinstance(function_call, str) and (
+                function_call == "none" or function_call == "auto"
+            ):
+                tool_choice = function_call
+            if isinstance(function_call, dict) and "name" in function_call:
+                tool_choice = {
+                    "type": "function",
+                    "function": {
+                        "name": function_call["name"],
+                    },
+                }
+
+        tool = None
+        if (
+            tool_choice is not None
+            and isinstance(tool_choice, dict)
+            and tools is not None
+        ):
+            name = tool_choice["function"]["name"]
+            tool = next((t for t in tools if t["function"]["name"] == name), None)
+            if tool is None:
+                raise ValueError(f"Tool choice '{name}' not found in tools.")
+            schema = tool["function"]["parameters"]
+            try:
+                # create grammar from json schema
+                grammar = llama_grammar.LlamaGrammar.from_json_schema(
+                    json.dumps(schema), verbose=llama.verbose
+                )
+            except Exception as e:
+                if llama.verbose:
+                    print(str(e), file=sys.stderr)
+                grammar = llama_grammar.LlamaGrammar.from_string(
+                    llama_grammar.JSON_GBNF, verbose=llama.verbose
+                )
+
+        completion_or_chunks = llama.create_completion(
+            prompt=prompt,
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            min_p=min_p,
+            typical_p=typical_p,
+            logprobs=top_logprobs if logprobs else None,
+            stream=stream,
+            stop=stop,
+            seed=seed,
+            max_tokens=max_tokens,
+            presence_penalty=presence_penalty,
+            frequency_penalty=frequency_penalty,
+            repeat_penalty=repeat_penalty,
+            tfs_z=tfs_z,
+            mirostat_mode=mirostat_mode,
+            mirostat_tau=mirostat_tau,
+            mirostat_eta=mirostat_eta,
+            model=model,
+            logits_processor=logits_processor,
+            grammar=grammar,
+            logit_bias=logit_bias,
+        )
+        if tool is not None:
+            tool_name = tool["function"]["name"]
+            return _convert_completion_to_chat_function(
+                tool_name, completion_or_chunks, stream
+            )
+        return _convert_completion_to_chat(completion_or_chunks, stream=stream)
 
 @register_chat_completion_handler("chatml-function-calling")
 def chatml_function_calling(
