796fa23135b98ce777f3867688dda42fba0bb934
abetlen <abetlen@gmail.com>
2026-06-07 15:51:32 -0700

    fix: remove stale pre-norm extension bindings

--- llama_cpp/llama_cpp_ext.py
+++ llama_cpp/llama_cpp_ext.py
@@ -42,63 +42,6 @@ def _ctypes_function_from_names(
     return decorator
 
 
-# LLAMA_API void llama_set_embeddings_pre_norm(struct llama_context * ctx, bool value, bool masked);
-@_ctypes_function_from_names(
-    (
-        "llama_set_embeddings_pre_norm",
-        "_Z29llama_set_embeddings_pre_normP13llama_contextbb",
-        "?llama_set_embeddings_pre_norm@@YAXPEAUllama_context@@_N1@Z",
-    ),
-    [llama_cpp.llama_context_p_ctypes, ctypes.c_bool, ctypes.c_bool],
-    None,
-)
-def llama_set_embeddings_pre_norm(
-    ctx: llama_cpp.llama_context_p,
-    value: bool,
-    masked: bool,
-    /,
-):
-    """Set whether the context outputs pre-norm embeddings or not."""
-    ...
-
-
-# LLAMA_API float * llama_get_embeddings_pre_norm(struct llama_context * ctx);
-@_ctypes_function_from_names(
-    (
-        "llama_get_embeddings_pre_norm",
-        "_Z29llama_get_embeddings_pre_normP13llama_context",
-        "?llama_get_embeddings_pre_norm@@YAPEAMPEAUllama_context@@@Z",
-    ),
-    [llama_cpp.llama_context_p_ctypes],
-    ctypes.POINTER(ctypes.c_float),
-)
-def llama_get_embeddings_pre_norm(
-    ctx: llama_cpp.llama_context_p,
-    /,
-):
-    """Get the pre-norm embeddings from the last evaluation."""
-    ...
-
-
-# LLAMA_API float * llama_get_embeddings_pre_norm_ith(struct llama_context * ctx, int32_t i);
-@_ctypes_function_from_names(
-    (
-        "llama_get_embeddings_pre_norm_ith",
-        "_Z33llama_get_embeddings_pre_norm_ithP13llama_contexti",
-        "?llama_get_embeddings_pre_norm_ith@@YAPEAMPEAUllama_context@@H@Z",
-    ),
-    [llama_cpp.llama_context_p_ctypes, ctypes.c_int32],
-    ctypes.POINTER(ctypes.c_float),
-)
-def llama_get_embeddings_pre_norm_ith(
-    ctx: llama_cpp.llama_context_p,
-    i: Union[ctypes.c_int32, int],
-    /,
-):
-    """Get the pre-norm embeddings for the ith output row from the last evaluation."""
-    ...
-
-
 # LLAMA_API void llama_set_embeddings_nextn(struct llama_context * ctx, bool value, bool masked);
 @_ctypes_function_from_names(
     (
